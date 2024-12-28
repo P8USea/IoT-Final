4f798ae9a1f2c3d987a0c215b65f65f1514a0f91
P8USea <funnylov32003@gmail.com>
2024-12-28 13:07:11 +0700

    Add callback conditions

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,33 @@
 #define IN1 26
 #define IN2 27
 #define SPEED 192
-#define MOISTURE_PIN 33
+#define MOISTURE_PIN 35
 void soil_Moisture_Handler();
 void doMotorStuff();
+void warning(char* topic, String message);
 namespace
 {
     const char *ssid = WiFiSecrets::ssid;
     const char *password = WiFiSecrets::pass;
-    const char *moisture_topic = "sensor/moisture";
+    const char *moisture_topic = "sensor/moisture/data";
     const char *humidity_topic = "sensor/humidity";
     const char *temperature_topic = "sensor/temperature";
     const char *motor_topic = "motor";
     const char *warning_topic = "warning";
 
     int data = 0;
-    
+    unsigned int warning_temp_up = 38;
+    unsigned int warning_temp_down = 10;
+    unsigned int warning_hum = 70;
+    unsigned int warning_moist_up = 30;
+    unsigned int warning_moist_down = 80; 
+
+    int reliability_count = 0; //Bien dem kiem tra do on dinh cua cam bien
+    int moisture_threshold = 70; //Nguong am de tuoi nuoc
+    int noise_threshold = 3; //Nguong sai so trong truong hop nhieu tin hieu
+    int sensor_last = 0;
+    long last_warning = millis();
+    boolean auto_flag = true;
 }
 
 WiFiClientSecure tlsClient;
@@ -50,17 +62,40 @@ void mqttCallback(char *topic, byte *payload, unsigned int length)
     Serial.printf("Message arrived from topic [%s]: %s\n", topic, message.c_str());
 
     //Xu ly thong tin cua cam bien
-    if(String(topic) == "sensor/moisture"){
+    if(String(topic) == "sensor/moisture/data"){
         soil_Moisture_Handler();
+    }
+    // Thay doi nguong do am tuoi nuoc
+    if(String(topic) == "sensor/moisture/change"){
+        moisture_threshold = message.toInt();
     }
 
     //Neu nhan thong tin tuoi nuoc cua nguoi dung
-    if (String(topic) == "motor") {
-        if(message == "ON"){
+    if (String(topic) == "motor/control" && message == "ON" && auto_flag) {
             doMotorStuff();
             Serial.println("Handy Watering...");
 
-        }
+    }
+    // Bat/tat tu dong tuoi nuoc
+    if(String(topic) == "motor/auto"){
+        auto_flag = bool(message);
+    }
+    if(millis() - last_warning > 60*1000){
+        //Canh bao nhiet do
+    if(String(topic) == "sensor/temperature" 
+    && (message.toFloat() > warning_temp_up || message.toFloat() < warning_temp_down)){
+        warning("warning/temp", "Temperature Issue!");
+    }
+    //Canh bao do am dat
+    if(String(topic) == "sensor/moisture" 
+    && (message.toFloat() > warning_moist_up || message.toFloat() < warning_moist_down)){
+        warning("warning/moisture","Moisture Issue!");
+    }
+    // Canh bao do am khong khi
+     if(String(topic) == "sensor/humidity" && message.toFloat() < warning_hum){
+        warning("warning/humidity", "Air Humidity Issue!");
+    }
+    last_warning = millis();
     }
 
 }
@@ -76,8 +111,8 @@ void mqttReconnect()
         {
             Serial.print(client_id);
             Serial.println(" connected");
-            mqttClient.subscribe(moisture_topic);
-            mqttClient.subscribe(motor_topic); //Dang ky motor_topic de nguoi dung dua ra quyet dinh tuoi nuoc
+            mqttClient.subscribe("sensor/#");
+            mqttClient.subscribe("motor/#");
 
         }
         else
@@ -92,10 +127,7 @@ void mqttReconnect()
 
 
 //MCU dua ra quyet dinh dua tren cam bien
-int reliability_count = 0; //Bien dem kiem tra do on dinh cua cam bien
-int moisture_threshold = 70; //Nguong am de tuoi nuoc
-int noise_threshold = 3; //Nguong sai so trong truong hop nhieu tin hieu
-int sensor_last = 0;
+
 void soil_Moisture_Handler(){
     int sensor_now = data;
     //Neu khong bi bien dong do nhieu trong 10 lan dem
@@ -156,6 +188,9 @@ void soil_Moisture_Reader(){
     mqttClient.publish(moisture_topic, String(data).c_str(), false);
 
 }
+void warning(char* topic, String message){
+    mqttClient.publish(topic, message.c_str(), false);
+}
 void setup()
 {
     Serial.begin(115200);
@@ -171,8 +206,8 @@ void setup()
     mqttClient.setCallback(mqttCallback);
     mqttClient.setServer(MQTT::broker, MQTT::port);
 
-    soil_Moisture_Publish_Ticker.attach(1, soil_Moisture_Reader);
-    DHT22_Publish_Ticker.attach(1, DHT22_Reader);
+    soil_Moisture_Publish_Ticker.attach(5, soil_Moisture_Reader);
+    DHT22_Publish_Ticker.attach(5, DHT22_Reader);
 }
 
 void loop()
